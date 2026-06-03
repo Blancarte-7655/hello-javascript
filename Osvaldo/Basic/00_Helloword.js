2b4166e52976bcfa51b50899689b420de385e62f
Blancarte-7655 <b.drum5677@gmail.com>
2026-06-03 17:20:36 -0600

    dia_1

---
+++ Osvaldo/Basic/00_Helloword.js
@@ -0,0 +1,33 @@
+console.log("Hola, JavaScript")
+console.log('Hola, JavaScript')
+console.log(`Hola, JavaScript`)
+
+console.log(5 + 3)
+console.log(5 - 3)
+console.log(5 * 3)
+console.log(5 / 3)
+console.log(5 % 3)
+console.log(5 ** 3)
+
+// Imprime el resultado de las operaciones matemáticas, sin concatenar con texto
+
+console.log("5 + 3 =", 5 + 3)
+console.log("5 - 3 =", 5 - 3)
+console.log("5 * 3 =", 5 * 3)
+console.log("5 / 3 =", 5 / 3)
+console.log("5 % 3 =", 5 % 3)
+console.log("5 ** 3 =", 5 ** 3)
+
+// Concatenación de cadenas y números, convierte los números a cadenas de texto
+
+console.log('5 + 3 = ' + (5 + 3))
+console.log('5 - 3 = ' + (5 - 3))
+console.log('5 * 3 = ' + (5 * 3))
+console.log('5 / 3 = ' + (5 / 3))
+console.log('5 % 3 = ' + (5 % 3))
+console.log('5 ** 3 = ' + (5 ** 3))
+
+// 
+
+
+
---
+++ Osvaldo/Basic/03_beginner_exercises.js
@@ -0,0 +1,122 @@
+// 1. Escribe un comentario en una línea
+
+// El monte everest no tiene nada contra de mi
+
+// 2. Escribe un comentario en varias líneas
+
+// El monte everest no tiene nada contra de mi
+// El monte everest no tiene nada contra de mi
+// El monte everest no tiene nada contra de mi
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let myName = "Blancarte"
+let age = 17
+let height = 1.82
+let isStudent = true
+let undefinedValue
+let nullValue = null
+let mySymbol = Symbol("triple_siete")
+let myBigInt = 777777777777777777777777777777777777777777n
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log("-".repeat(80))
+console.log('4.')
+console.log("-".repeat(80))
+
+console.log(myName)
+console.log(age)
+console.log(height)
+console.log(isStudent)
+console.log(undefinedValue)
+console.log(nullValue)
+console.log(mySymbol)
+console.log(myBigInt)
+
+// 5. Imprime por consola el tipo de todas las variables
+
+console.log("-".repeat(80))
+console.log('5.')
+console.log("-".repeat(80))
+
+console.log(typeof myName)
+console.log(typeof age)
+console.log(typeof height)
+console.log(typeof isStudent)
+console.log(typeof undefinedValue)
+console.log(typeof nullValue)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+myName = "Osvaldo"
+age = 18
+height = 1.81
+isStudent = false
+undefinedValue = undefined
+nullValue = null
+mySymbol = Symbol("triple_nueve")
+myBigInt = 99999999999999999999999999999999999999n
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+myName = 777
+age = "dieciocho"
+height = true
+isStudent = 1.81
+undefinedValue = null
+nullValue = undefined
+mySymbol = "triple_nueve"
+myBigInt = Symbol("triple_nueve")
+
+console.log("-".repeat(80))
+console.log('7.')
+console.log("-".repeat(80))
+
+console.log(myName)
+console.log(age)
+console.log(height)
+console.log(isStudent)
+console.log(undefinedValue)
+console.log(nullValue)
+console.log(mySymbol)
+console.log(myBigInt)
+
+console.log("-".repeat(80))
+console.log('7. Tipos de datos después de modificar los valores por otros de distinto tipo')
+console.log("-".repeat(80))
+
+console.log(typeof myName)
+console.log(typeof age)
+console.log(typeof height)
+console.log(typeof isStudent)
+console.log(typeof undefinedValue)
+console.log(typeof nullValue)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const myName2 = "Osvaldo"
+const age2 = 18
+const height2 = 1.82
+const isStudent2 = false
+const undefinedValue2 = undefined
+const nullValue2 = null
+const mySymbol2 = Symbol("triple_siete")
+const myBigInt2 = 777777777777777777777777777777777777777n
+
+// 9. A continuación, modifica los valores de las constantes
+
+// myName2 = "Blancarte"
+// age2 = 17
+// height2 = 1.82
+// isStudent2 = true
+// undefinedValue2 = undefined
+// nullValue2 = null
+// mySymbol2 = Symbol("triple_nueve")
+// myBigInt2 = 99999999999999999999999999999999999999n
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
